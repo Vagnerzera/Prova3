755806142d6d5eec56f55f90cc721b167787565b
Vagnerzera <gabrielvagner2@gmail.com>
2022-06-08 14:38:38 -0300

    Initial commit

---
+++ src/app/products.ts
@@ -0,0 +1,48 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Ifonso 12',
+    price: 600,
+    description:
+      'Um celular top de linha, com vários features para todos os gostos',
+  },
+  {
+    id: 2,
+    name: 'Galaxi X65',
+    price: 999,
+    description: 'Um otimo celular, com uma ótima camera.',
+  },
+  {
+    id: 3,
+    name: 'Nokia Tijolão',
+    price: 2.99,
+    description:
+      'O classico celular, utilizando em construçoes devido a sua alta resistencia',
+  },
+  {
+    id: 4,
+    name: 'Telefone do batman',
+    price: 5000000,
+    description:
+      'Melhor telefone que existe no universo, possui 800gb de ram, um i27 de 309 geração e um SSD de 1 zetabyte',
+  },
+  {
+    id: 5,
+    name: 'Metade do planeta terra',
+    price: 99999999999999999999,
+    description: 'Em promoção!!',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
